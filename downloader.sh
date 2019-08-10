da3092c8716c45617db13368685a4d4058158542
Ahmad Tolba <tolpa1@gmail.com>
2019-08-11 00:29:47 +0200

    Flush the arry of ids after downloading the articles

--- downloader.sh
+++ downloader.sh
@@ -16,9 +16,10 @@ prog() {
 get_articles(){
   echo "Started downloading...."
   local number_of_pages=84
-  local ids=()
 
   for number in $(seq 1 $number_of_pages); do
+    local ids=()
+
     prog "$number"
 
     ids+=$(curl -ks https://api.manshar.com/api/v1/articles?page=$number | jq --raw-output '.[].id')
@@ -26,6 +27,8 @@ get_articles(){
     for article_id in $ids; do
       curl -ks https://api.manshar.com/api/v1/articles/$article_id -o $article_id.json
     done
+
+    unset ids
   done
 
 }
