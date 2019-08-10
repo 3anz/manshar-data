5e9e6c8879a92328ac02dfa9384bd7f76cd2f8aa
Ahmad Tolba <tolpa1@gmail.com>
2019-08-11 00:19:29 +0200

    Add progress bar

--- downloader.sh
+++ downloader.sh
@@ -3,6 +3,15 @@
 set -e
 set -o pipefail
 
+# Stolen from: https://unix.stackexchange.com/a/415450/162906
+prog() {
+    local w=80 p=$1;  shift
+    # create a string of spaces, then change them to dots
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /.};
+    # print those dots on a fixed-width space plus the percentage etc. 
+    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*"; 
+}
+
 # Shitty function to download the articles
 get_articles(){
   echo "Started downloading...."
@@ -10,6 +19,8 @@ get_articles(){
   local ids=()
 
   for number in $(seq 1 $number_of_pages); do
+    prog "$number"
+
     ids+=$(curl -ks https://api.manshar.com/api/v1/articles?page=$number | jq --raw-output '.[].id')
 
     for article_id in $ids; do
